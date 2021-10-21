2a7cdeb9b078e9bf76640732846d7ff0617f6ae4
shyunju7 <shyunju7@naver.com>
2021-10-21 13:29:18 +0900

    Feat connecting DB

--- hi-nest/src/app.module.ts
+++ hi-nest/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
 import { AppController } from './app.controller';
 import { AppService } from './app/app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [MoviesModule, TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
